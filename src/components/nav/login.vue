40016c2dae3aa88ba865d5a1ff6640ff55f44316
zq <zq@qq.com>
2019-03-21 14:54:00 +0800

    init file

---
+++ src/components/nav/login.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <headBar></headBar>
+    <div class="login_bg">
+      <div class="login_box">
+        <input
+          class="username"
+          type="text"
+          placeholder="用户名："
+          ref="user"
+        >
+        <input
+          class="pwd"
+          type="password"
+          placeholder="密码："
+          ref="pwd"
+        >
+        <div class="login_btn" @click="register">登录</div>
+      </div>
+    </div>
+    <footerBar class="footerBar"></footerBar>
+  </div>
+</template>
+<script>
+// import loginBg from "../assets/images/login_bg.png"
+import headBar from "@/components/nav/header";
+import footerBar from "@/components/nav/footer";
+export default {
+  data() {
+    return {
+     
+    };
+  },
+  mounted(){
+    // console.log(this.$axios);
+  },
+  methods:{
+    register:function(){
+      let user = this.$refs.user.value;
+      let pwd = this.$refs.pwd.value;
+      console.log(user+"  "+pwd)
+      let url = 'http://localhost:80/front/froUser/save';
+      this.$axios.post(url,{
+        userName:user,
+        password:pwd
+      }).then(function(res){
+        console.log(res);
+      }).catch(function(error){
+        console.log(error);
+      })
+    }
+  },
+  components: {
+    headBar,
+    footerBar
+  },
+};
+</script>
+
+<style>
+.login_bg {
+    width: 100%;
+    height: 500px;
+    background: url("../../assets/images/login_bg.jpg") no-repeat;
+    background-size: 100% 100%;
+    position: absolute;
+    margin-top: 10px;
+}
+.login_box {
+  width: 300px;
+  height: 300px;
+  margin: auto;
+  margin-right: 200px;
+  background: #f6f6f7;
+  border-radius: 10px;
+  margin-top: 100px;
+  margin-bottom: 100px;
+}
+.login_box input {
+  width: 240px;
+  height: 30px;
+  margin-left: 20px;
+  margin-top: 30px;
+  outline: none;
+  background: #ffffff;
+  color: #666666;
+  padding-left: 10px;
+}
+.login_box input:first-child {
+  margin-top: 60px;
+}
+.login_btn {
+  width: 240px;
+  height: 30px;
+  background: #40b6fc;
+  margin: 30px;
+  color: #ffffff;
+  text-align: center;
+  line-height: 30px;
+  border-radius: 5px;
+}
+.footerBar {
+    margin-top: 520px;
+}
+</style>
+
---
+++ src/components/nav/news.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="news_body">
+     <div class="menu_header">
+      <headBar></headBar>
+    </div>
+    <div class="new_box">
+      <div class="news_list">
+        <span>山东|2019年山东曲阜市引进优秀i青年人才100人公告</span>
+        <div class="news_mes">
+          <span>200人浏览</span>
+          <span>03-21</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import headBar from "@/components/nav/header";
+import footerBar from "@/components/nav/footer";
+export default {
+  
+  components: {
+    headBar,
+    footerBar
+  }
+};
+</script>
+
+<style>
+.menu_header {
+  width: 80%;
+  margin: auto;
+}
+.new_box {
+  width: 100%;
+  border: 1px solid;
+  margin-top: 10px;
+}
+.news_list {
+  width: 80%;
+  margin: auto;
+  height: 30px;
+  line-height: 30px;
+  font-size: 14px;
+  color: #666666;
+  overflow: hidden;
+}
+.news_mes {
+  float: right;
+}
+.news_mes span:first-child {
+  padding-right: 20px;
+}
+</style>
+
