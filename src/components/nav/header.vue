49b17ecc320c26f1827bc44e283eb4ea9463afa7
zhangxueyan8 <zxy_3300607>
2019-03-22 18:19:03 +0800

    修改链接跳转方式

--- src/components/nav/header.vue
+++ src/components/nav/header.vue
@@ -6,17 +6,17 @@
       mode="horizontal"
       @select="handleSelect"
     >
-      <el-menu-item index="1"><a href="/">首页</a></el-menu-item>
+      <el-menu-item index="1" @click="goIndex(1)">首页</el-menu-item>
       <el-submenu index="2">
         <template slot="title">学习资源</template>
-        <el-menu-item index="2"><a href="/learnData">学习资料</a></el-menu-item>
-        <el-menu-item index="2"><a href="/learnVideo">在线视频</a></el-menu-item>
+        <el-menu-item index="2" @click="goIndex(2)">学习资料</el-menu-item>
+        <el-menu-item index="2" @click="goIndex(3)">在线视频</el-menu-item>
       </el-submenu>
-      <el-menu-item index="3"><a href="/news">站内新闻</a></el-menu-item>
+      <el-menu-item index="3" @click="goIndex(4)">站内新闻</el-menu-item>
       <el-submenu index="4">
         <template slot="title">在线测试</template>
-        <el-menu-item index="4"><a href="/xingceMenu">行测测试</a></el-menu-item>
-        <el-menu-item index="4"><a href="/shenlunMenu">申论测试</a></el-menu-item>
+        <el-menu-item index="4" @click="goIndex(5)">行测测试</el-menu-item>
+        <el-menu-item index="4" @click="goIndex(6)">申论测试</el-menu-item>
       </el-submenu>
       <el-menu-item index="5"><a href="/login">登录</a>/<a href="/login">注册</a></el-menu-item>
     </el-menu>
@@ -34,8 +34,20 @@ export default {
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
     },
-    change(index){
-      this.activeIndex = index;
+    goIndex(num){
+      if(num == 1){
+        window.location.href = '/';
+      }else if(num == 2){
+        window.location.href = '/learnData';
+      }else if(num == 3){
+        window.location.href = '/learnVideo';
+      }else if(num == 4){
+        window.location.href = '/news';
+      }else if(num == 5){
+        window.location.href = '/xingceMenu';
+      }else if(num == 6){
+        window.location.href = '/shenlunMenu';
+      }
     }
   }
 };
